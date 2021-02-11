01bbe1da48793400ed64edf1fef6efeb2c5e9d09
minchan <55829146+flux1126@users.noreply.github.com>
2021-02-12 01:31:19 +0900

    Create folders

--- snugh/user/views.py
+++ snugh/user/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from rest_framework import status, viewsets
-from django.contrib.auth.models import User
 
-class UserViewSet(viewsets.GenericViewSet):
-    queryset=User.objects.all()
-#    serializer_class=
-    permisison_classes=(IsAuthenticated(), )
-
-    def 
+# Create your views here.
