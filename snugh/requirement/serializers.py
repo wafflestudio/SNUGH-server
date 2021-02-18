aae1d3fd1dfee8ed589054d60a19c72347c18f01
minchan <55829146+flux1126@users.noreply.github.com>
2021-02-18 21:38:43 +0900

    Add update function and progress function to requirement/views.py

--- snugh/requirement/serializers.py
+++ snugh/requirement/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from requirement.models import Requirement, PlanRequirement
+from lecture.models import Plan
 
 class RequirementSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    is_credit_requirement = serializers.SerializerMethodField()
+    requirement_type = serializers.SerializerMethodField()
+    requirement_type_detail = serializers.SerializerMethodField()
+    requirement_type_detail_detail = serializers.SerializerMethodField()
+    required_credit = serializers.SerializerMethodField()
 
     class Meta:
-        model = Requirement
+        Model = PlanRequirement
         fields = (
             'id',
             'description',
@@ -17,10 +25,66 @@ class RequirementSerializer(serializers.ModelSerializer):
             'earned_credit'
         )
 
-class ProgressSerializer(serializers.ModelSerializer):
+    def get_id(self, plan_requirement):
+        return plan_requirement.plan_id
+
+    def get_description(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.description
+
+    def get_is_credit_requirement(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.is_credit_requirement
+
+    def get_requirement_type(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.requirement_type
+
+    def get_requirement_type_detail(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.requirement_type_detail
+
+    def get_requirement_type_detail_detail(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.requirement_type_detail_detail
+
+    def get_required_credit(self, plan_requirement):
+        requirement = Requirement.objects.get(pk=plan_requirement.plan_id)
+        return requirement.required_credit
+
+class ProgressSerializer(serializers.ModelSerializer): # 미완성
+    all = serializers.SerializerMethodField()
+    major_requirement = serializers.SerializerMethodField()
+    major_elective = serializers.SerializerMethodField()
+    general = serializers.SerializerMethodField()
 
     class Meta:
-        model = PlanRequirement
+        Model = Plan
+        fields = (
+            'all',
+            'major_requirement',
+            'major_elective',
+            'general'
+        )
+
+    def get_all(self, plan):
+        return None
+
+    def get_major_requirement(self, plan):
+        return None
+
+    def get_major_elective(self, plan):
+        return None
+
+    def get_general(self, plan):
+        return None
+
+class ProgressDetailSerializer(serializers.ModelSerializer): # 미완성
+    required_credit = serializers.SerializerMethodField()
+    earned_credit = serializers.SerializerMethodField()
+    progress = serializers.SerializerMethodField()
+
+    class Meta:
         fields = (
             'required_credit',
             'earned_credit',
--- snugh/requirement/views.py
+++ snugh/requirement/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from rest_framework import status, viewsets, filters
 from rest_framework.response import Response
 from requirement.models import Requirement, PlanRequirement
 from requirement.serializers import RequirementSerializer, ProgressSerializer
 from lecture.models import Plan
+
 
 class RequirementViewSet(viewsets.GenericViewSet):
     queryset = Requirement.objects.all()
@@ -14,6 +14,8 @@ class RequirementViewSet(viewsets.GenericViewSet):
         user = request.user
         if not user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        requirements = self.get_object()
 
         plan_id = request.query_params.get('plan_id')
         search_type = request.query_params.get('type')
@@ -27,20 +29,56 @@ class RequirementViewSet(viewsets.GenericViewSet):
             plan = Plan.objects.get(pk=plan_id)
 
             if search_type == "all":
-                requirements = None # 작업중
+                plan_requirements = PlanRequirement.objects.filter(plan_id=plan_id)
             elif search_type == "credit":
-                requirements = None # 작업중
+                requirements = requirements.planrequirement.filter(plan_id=plan_id)
+                requirements = requirements.filter(is_credit_requirement=True)
+                plan_requirements = requirements.planrequirement
             elif search_type == "etc":
-                requirements = None # 작업중
+                requirements = requirements.planrequirement.filter(plan_id=plan_id)
+                requirements = requirements.filter(is_credit_requirement=False)
+                plan_requirements = requirements.planrequirement
             else:
                 return Response({"error": "wrong search_type."}, status=status.HTTP_400_BAD_REQUEST)
         except Plan.DoesNotExist:
             return Response({"error": "wrong plan_id"}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = self.get_serializer(requirements, many=True)
+        serializer = self.get_serializer(plan_requirements, many=True)
         data = serializer.data
         return Response(data, status=status.HTTP_200_OK)
 
     # PUT /requirement
+    def update(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        data = request.data.copy()
+
+        plan_requirements = PlanRequirement.objects.all()
+        serializer = RequirementSerializer(plan_requirements, data=data, many=True, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.update(plan_requirements, serializer.validated_data)
+
+        data = serializer.data
+        return Response(data, status=status.HTTP_200_OK)
 
     # GET /requirement/progress
+    def progress(self, request):
+        user = request.user
+        if not user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        plan_id = request.query_params.get('plan_id')
+
+        if not plan_id:
+            return Response({"error": "plan_id missing."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            plan = Plan.objects.get(pk=plan_id)
+        except Plan.DoesNotExist:
+            return Response({"error": "wrong plan_id"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProgressSerializer(plan)
+        data = serializer.data
+        return Response(data, status=status.HTTP_200_OK)
