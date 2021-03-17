9fb8d1d0b526592b1dda211fe1837f273c40c0ba
minchan <55829146+flux1126@users.noreply.github.com>
2021-03-18 01:46:23 +0900

    Update user from integer choice field to char choice field

--- snugh/user/models.py
+++ snugh/user/models.py
@@ -1,34 +1,49 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# UserProfile, Major, UserMajor
 
+# UserProfile, Major, UserMajor
 class UserProfile(models.Model):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    BREAK = 'break'
+
     STUDENT_STATUS = (
-        (1, 'active'),# 재학 
-        (2, 'inactive'),# 서비스 탈퇴  
-        (3, 'break'),# 휴학         
+        (ACTIVE, 'active'),  # 재학
+        (INACTIVE, 'inactive'),  # 서비스 탈퇴
+        (BREAK, 'break'),  # 휴학
     )
     user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
     year = models.IntegerField()
-    status = models.PositiveSmallIntegerField(choices=STUDENT_STATUS, default=1)
+    status = models.CharField(choices=STUDENT_STATUS, default=ACTIVE)
+
 
 class Major(models.Model):
+    DOUBLE_MAJOR = 'double_major'
+    MINOR = 'minor'
+    INTERDISCIPLINARY_MAJOR = 'interdisciplinary_major'
+    INTERDISCIPLINARY = 'interdisciplinary'
+    SINGLE_MAJOR = 'single_major'
+    INTERDISCIPLINARY_MAJOR_FOR_TEACHER = 'interdisciplinary_major_for_teacher_training_programs'
+    STUDENT_DIRECTED_MAJOR = 'student_directed_major'
+
     MAJOR_TYPE = (
-        (1, 'double_major'),# 복수전공 
-        (2, 'minor'),# 부전공 
-        (3, 'interdisciplinary_major'),# 연합전공 
-        (4, 'interdisciplinary'),# 연계전공 
-        (5, 'single_major'),# 단일전공 
-        (6, 'interdisciplinary_major_for_teacher_training_programs'),# 교직연합전공 
-        (7, 'student_directed_major'),# 학생설계전공 
+        (DOUBLE_MAJOR, 'double_major'),  # 복수전공
+        (MINOR, 'minor'),  # 부전공
+        (INTERDISCIPLINARY_MAJOR, 'interdisciplinary_major'),  # 연합전공
+        (INTERDISCIPLINARY, 'interdisciplinary'),  # 연계전공
+        (SINGLE_MAJOR, 'single_major'),  # 단일전공
+        (INTERDISCIPLINARY_MAJOR_FOR_TEACHER, 'interdisciplinary_major_for_teacher_training_programs'),  # 교직연합전공
+        (STUDENT_DIRECTED_MAJOR, 'student_directed_major'),  # 학생설계전공
     )
     major_name = models.CharField(max_length=50, db_index=True)
     major_type = models.PositiveSmallIntegerField(choices=MAJOR_TYPE)
 
+
 class UserMajor(models.Model):
     user = models.ForeignKey(User, related_name='usermajor', on_delete=models.CASCADE)
     major = models.ForeignKey(Major, related_name='usermajor', on_delete=models.CASCADE)
+
     class Meta:
         unique_together = (
             ('user', 'major')
