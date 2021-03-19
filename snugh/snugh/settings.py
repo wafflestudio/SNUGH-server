20d15dce23d7e89366460f9d5746b7b775105102
Jonghyun Choe <andy.choe729@gmail.com>
2021-03-19 19:53:52 +0900

    Revert back databases setting

--- snugh/snugh/settings.py
+++ snugh/snugh/settings.py
@@ -120,37 +120,26 @@ WSGI_APPLICATION = 'snugh.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost', 
-        'PORT': 3306,
-        'NAME': 'snugh_server',
-        'USER': 'snugh-server',
-        'PASSWORD': 'baby_ammo',
+        'NAME': 'snugh_db',
+        'USER': 'admin',
+        'PASSWORD': 'babyammo',
+        'HOST': 'snugh-db.cey1mulmuayy.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'snugh_db',
-#         'USER': 'admin',
-#         'PASSWORD': 'babyammo',
-#         'HOST': 'snugh-db.cey1mulmuayy.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
-#         }
-#     }
-# }
-
-# CACHES = {
-#         "default": {
-#         "BACKEND" : "django_redis.cache.RedisCache",
-#         "LOCATION" : "redis://127.0.0.1",
-#         "OPTIONS": {
-#             "CLIENT_CLASS" : "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
+CACHES = {
+        "default": {
+        "BACKEND" : "django_redis.cache.RedisCache",
+        "LOCATION" : "redis://127.0.0.1",
+        "OPTIONS": {
+            "CLIENT_CLASS" : "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 
 # Password validation
