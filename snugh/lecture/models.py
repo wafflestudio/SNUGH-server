b99081ffdb7dda98e135cf76f99a9e5f5e2aae66
minchan <55829146+flux1126@users.noreply.github.com>
2021-03-19 20:02:53 +0900

    Fix naive error

--- snugh/lecture/models.py
+++ snugh/lecture/models.py
@@ -104,8 +104,8 @@ class SemesterLecture(models.Model):
         (MAJOR_REQUIREMENT, 'major_requirement'),
         (MAJOR_ELECTIVE, 'major_elective'),
         (GENERAL, 'general'),
-        (GENERAL_ELECTIVE, 'general_elective')
-        (TEACHING, 'teaching')
+        (GENERAL_ELECTIVE, 'general_elective'),
+        (TEACHING, 'teaching'),
     )
     LECTURE_TYPE_DETAIL = (
         (NONE, 'none'),
