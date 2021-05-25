cd7f7d8cf05a0fa844d95666a9c0f6693f7dddd9
minchan <55829146+flux1126@users.noreply.github.com>
2021-05-25 20:59:45 +0900

    Update Requirement Model

--- snugh/requirement/models.py
+++ snugh/requirement/models.py
@@ -16,25 +16,6 @@ class Requirement(models.Model):
     TEACHING = 'teaching'  # 교직
     ALL = 'all'  # 전체
 
-    # Requirement Type Detail
-    BASE_OF_STUDY = 'base_of_study'  # 학문의 기초
-    WORLD_OF_STUDY = 'world_of_study'  # 학문의 세계
-    OTHER = 'other'  # 일반 교양
-
-    # Requirement Type Detail Detail
-    THOUGHT_AND_EXPRESSION = 'thought_and_expression'  # 사고와 표현
-    FOREIGN_LANGUAGES = 'foreign_languages'  # 외국어
-    MATHEMATICAL_ANALYSIS_AND_REASONING = 'mathematical_analysis_and_reasoning'  # 수량적 분석과 추론
-    SCIENTIFIC_THINKING_AND_EXPERIMENT = 'scientific_thinking_and_experiment'  # 과학적 사고와 실험
-    COMPUTER_AND_INFORMATICS = 'computer_and_informatics'  # 컴퓨터와 정보활용
-    LANGUAGE_AND_LITERATURE = 'language_and_literature'  # 언어와 문학
-    CULTURE_AND_ARTS = 'culture_and_arts'  # 문화와 예술
-    HISTORY_AND_PHILOSOPHY = 'history_and_philosophy'  # 역사와 철학
-    POLITICS_AND_ECONOMICS = 'politics_and_economics'  # 정치와 경제
-    HUMANITY_AND_SOCIETY = 'humanity_and_society'  # 인간과 사회
-    NATURE_AND_TECHNOLOGY = 'nature_and_technology'  # 자연과 기술
-    LIFE_AND_ENVIRONMENT = 'life_and_environment'  # 생명과 환경
-
     REQUIREMENT_TYPE = (
         (NONE, 'none'),
         (MAJOR_REQUIREMENT, 'major_requirement'),
@@ -44,27 +25,6 @@ class Requirement(models.Model):
         (TEACHING, 'teaching'),
         (ALL, 'all'),
     )
-    REQUIREMENT_TYPE_DETAIL = (
-        (NONE, 'none'),
-        (BASE_OF_STUDY, 'base_of_study'),
-        (WORLD_OF_STUDY, 'world_of_study'),
-        (OTHER, 'other'),
-    )
-    REQUIREMENT_TYPE_DETAIL_DETAIL = (
-        (NONE, 'none'),  # 구분 없음
-        (THOUGHT_AND_EXPRESSION, 'thought_and_expression'),  # 사고와 표현
-        (FOREIGN_LANGUAGES, 'foreign_languages'),  # 외국어
-        (MATHEMATICAL_ANALYSIS_AND_REASONING, 'mathematical_analysis_and_reasoning'),  # 수량적 분석과 추론
-        (SCIENTIFIC_THINKING_AND_EXPERIMENT, 'scientific_thinking_and_experiment'),  # 과학적 사고와 실험
-        (COMPUTER_AND_INFORMATICS, 'computer_and_informatics'),  # 컴퓨터와 정보활용
-        (LANGUAGE_AND_LITERATURE, 'language_and_literature'),  # 언어와 문학
-        (CULTURE_AND_ARTS, 'culture_and_arts'),  # 문화와 예술
-        (HISTORY_AND_PHILOSOPHY, 'history_and_philosophy'),  # 역사와 철학
-        (POLITICS_AND_ECONOMICS, 'politics_and_economics'),  # 정치와 경제
-        (HUMANITY_AND_SOCIETY, 'humanity_and_society'),  # 인간과 사회
-        (NATURE_AND_TECHNOLOGY, 'nature_and_technology'),  # 자연과 기술
-        (LIFE_AND_ENVIRONMENT, 'life_and_environment'),  # 생명과 환경
-    )
     major = models.ForeignKey(Major, related_name='requirement', on_delete=models.CASCADE)
     start_year = models.PositiveSmallIntegerField()
     end_year = models.PositiveSmallIntegerField()
@@ -72,8 +32,6 @@ class Requirement(models.Model):
     is_credit_requirement = models.BooleanField()
     required_credit = models.PositiveSmallIntegerField(default=0)
     requirement_type = models.CharField(max_length=50, choices=REQUIREMENT_TYPE)
-    requirement_type_detail = models.CharField(max_length=50, choices=REQUIREMENT_TYPE_DETAIL, default=NONE)
-    requirement_type_detail_detail = models.CharField(max_length=50, choices=REQUIREMENT_TYPE_DETAIL_DETAIL, default=NONE)
 
     class Meta:
         ordering = ['-end_year', '-start_year']  # 최신순
