1832ad2e50ed245de4d7cdc478e4ba18bc853aeb
Jaewoong Kim <rorhcdream@gmail.com>
2021-09-26 11:46:14 +0900

    Create category field to FAQ
    * Create category field to FAQ
    * Create category query param to GET /faq

--- snugh/faq/models.py
+++ snugh/faq/models.py
@@ -5,6 +5,7 @@ class FAQ(models.Model):
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=500)
     read_count = models.IntegerField(default=0)
+    category = models.CharField(max_length=50, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
