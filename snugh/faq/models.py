0475c184564235ea4ffcb005ce7348881331811c
Jaewoong Kim <rorhcdream@gmail.com>
2021-09-26 11:26:18 +0900

    Make FAQ list ordered by read_count
    * Create read_count field to FAQ model
    * Create FAQ list ordering feature (default: -read_count)
    * Create Get /faq/{id} API endpoint to track read count

--- snugh/faq/models.py
+++ snugh/faq/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class FAQ(models.Model):
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=500)
+    read_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
--- snugh/faq/views.py
+++ snugh/faq/views.py
@@ -10,6 +10,7 @@ class FAQViewSet(viewsets.GenericViewSet):
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
 
+    # POST /faq
     def create(self, request):
         body = request.data
         question = body.get('question')
@@ -18,14 +19,26 @@ class FAQViewSet(viewsets.GenericViewSet):
         serializer = self.get_serializer(faq)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    # GET /faq
     def list(self, request):
+        default_order = '-read_count'
         page = request.GET.get('page', '1')
-        faqs = self.get_queryset().order_by('created_at')
+        order = request.GET.get('order', default_order)
+        faqs = self.get_queryset().order_by(order)
         faqs = Paginator(faqs, 5).get_page(page)
         serializer = self.get_serializer(faqs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # DEL /faq/{id}
     def delete(self, request, pk=None):
         faq = get_object_or_404(FAQ, pk=pk)
         faq.delete()
         return Response(status=status.HTTP_200_OK)
+
+    # GET /faq/{id}
+    def retrieve(self, request, pk=None):
+        faq = get_object_or_404(FAQ, pk=pk)
+        faq.read_count += 1
+        faq.save()
+        serializer = self.get_serializer(faq)
+        return Response(serializer.data, status=status.HTTP_200_OK)
