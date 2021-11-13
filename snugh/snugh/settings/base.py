6fbf82261522fd257b15dc3dacc7c486f3c6cfe3
Minchan Kim <55829146+shoveling-pig@users.noreply.github.com>
2021-11-13 16:15:00 +0900

    feat: find password

--- snugh/snugh/settings/base.py
+++ snugh/snugh/settings/base.py
@@ -160,3 +160,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_PORT = '587'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'snugh.wafflestudio@gmail.com'
+EMAIL_HOST_PASSWORD = 'babyammo'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- snugh/user/urls.py
+++ snugh/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from django.contrib.auth import views as auth_views
 from user.views import UserViewSet, MajorViewSet
 
 
@@ -11,4 +12,8 @@ router.register('major', MajorViewSet, basename='major')
 
 urlpatterns = [
     path('', include((router.urls))),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
