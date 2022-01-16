50ada87358df303eaac509300f400146d330745e
kmy17518 <69717995+kmy17518@users.noreply.github.com>
2022-01-16 13:29:15 +0900

    create departmentequivalent majorequivalent
    1. 학과가 1-2개여서 학부단위로 개설되는 수업
    2. 학부 내에 학부가 있고 그 안에서 세부전공으로 나눠지는 경우
    를 처리하도록 변경

--- snugh/user/models.py
+++ snugh/user/models.py
@@ -63,6 +63,20 @@ class MajorDepartment(models.Model):
             ('department', 'major')
         )
 
+
+# 간호학과, 경영학과, 약학과, 제약학과(2+4년제), 의예과, 의학과, 자유전공학부
+class DepartmentEquivalent(models.Model):
+    major_name = models.CharField(max_length=50)
+    department_name = models.CharField(max_length=50)
+
+# 정치외교학부 외교학과
+# 디자인학부 디자인과 공예과
+# 소비자아동학부 소비자학과 아동가족학과
+class MajorEquivalent(models.Model):
+    major_name = models.CharField(max_length=50)
+    equivalent_major_name = models.CharField(max_length=50)
+
+
 class UserMajor(models.Model):
     user = models.ForeignKey(User, related_name='usermajor', on_delete=models.CASCADE)
     major = models.ForeignKey(Major, related_name='usermajor', on_delete=models.CASCADE)
