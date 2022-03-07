19a36158211fb28c7ab472434798b20f6cd21d6b
Jaewan Park <jaejae1112@gmail.com>
2022-03-08 00:03:38 +0900

    fix: user tests

--- snugh/snugh/settings/test.py
+++ snugh/snugh/settings/test.py
@@ -1,3 +1,3 @@
-from local import *
+from .local import *
 
 TEST_RUNNER = 'snugh.test_runner.TestRunner'
--- snugh/user/tests.py
+++ snugh/user/tests.py
@@ -1,17 +1,15 @@
 from factory.django import DjangoModelFactory
 
 from django.test import TestCase
-from django.db import transaction
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from django.contrib.auth.models import User
 from user.models import UserProfile, Major, UserMajor
 
-class UserFactory(DjangoModelFactory):
+class UserTestFactory(DjangoModelFactory):
     class Meta:
         model = UserProfile
-
-    email = 'test@test.com'
 
     @classmethod
     def create(cls, **kwargs):
@@ -32,13 +30,16 @@ class UserFactory(DjangoModelFactory):
         userprofile = UserProfile.objects.create(user=user, entrance_year=entrance_year, status=student_status)
         userprofile.save()
 
-        return userprofile
+        token, created = Token.objects.get_or_create(user=user)
 
-class CreateUserTestCase(TestCase):
+        return user
+
+
+class UserTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = UserFactory(
-            email='test@test.com',
+        cls.user = UserTestFactory(
+            email='testuser@test.com',
             password='password',
             entrance_year=2022,
             full_name='홍길동',
@@ -55,8 +56,10 @@ class CreateUserTestCase(TestCase):
             status='active'
         )
 
+        cls.user_token = "Token " + str(cls.user.auth_token)
+
         cls.post_data = {
-            'email': 'user@test.com',
+            'email': 'member@test.com',
             'password': 'password',
             'entrance_year': 2022,
             'full_name': '아무개',
@@ -73,9 +76,14 @@ class CreateUserTestCase(TestCase):
             'status': 'active'
         }
 
+        cls.put_data = {
+            'email': 'testuser@test.com',
+            'password': 'password',
+        }
+
     def test_create_user_missing_requirements(self):
         data = {'email': 'test@test.com'}
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "password, entrance_year, full_name, status, majors missing")
@@ -83,43 +91,43 @@ class CreateUserTestCase(TestCase):
     def test_create_user_wrong_request(self):
         data = self.post_data.copy()
         data.update({'email': 1})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "email wrong_type")
 
         data = self.post_data.copy()
         data.update({'email': "wrong email"})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "email wrong_format")
 
         data = self.post_data.copy()
         data.update({'password': "false"})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "password wrong_range(more than 6 letters)")
 
         data = self.post_data.copy()
         data.update({'entrance_year': 100})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "entrance_year wrong_range(4 digits)")
 
         data = self.post_data.copy()
         data.update({'full_name': "a"})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "full_name wrong_range(2~30 letters)")
 
     def test_create_user_email_already_exists(self):
         data = self.post_data.copy()
-        data.update({'email': "test@test.com"})
-        response = self.client.post('/user/', data=data)
+        data.update({'email': "testuser@test.com"})
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "email already_exist")
@@ -127,33 +135,33 @@ class CreateUserTestCase(TestCase):
     def test_create_user_major_error(self):
         data = self.post_data.copy()
         data.update({'majors': []})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "majors missing")
 
         data = self.post_data.copy()
         data.update({'majors': [{"major_name": "경영학과", "major_type": "double_major"}]})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "major_type not_allowed")
 
         data = self.post_data.copy()
         data.update({'majors': [{"major_name": "경영학과", "major_type": "double_major"}, {"major_name": "컴퓨터공학부", "major_type": "double_major"}]})
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "major_type not_allowed")
 
     def test_create_user(self):
         data = self.post_data
-        response = self.client.post('/user/', data=data)
+        response = self.client.post('/user/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         body = response.json()
         self.assertIn("id", body)
-        self.assertEqual(body["email"], "user@test.com")
+        self.assertEqual(body["email"], "member@test.com")
         self.assertEqual(body["entrance_year"], 2022)
         self.assertEqual(body["full_name"], "아무개")
         self.assertIn("majors", body)
@@ -161,36 +169,9 @@ class CreateUserTestCase(TestCase):
         self.assertEqual(body["status"], "active")
         self.assertIn("token", body)
 
-
-class LoginTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory(
-            email='test@test.com',
-            password='password',
-            entrance_year=2022,
-            full_name='홍길동',
-            majors=[
-                {
-                    "major_name": "컴퓨터공학부",
-                    "major_type": "major"
-                },
-                {
-                    "major_name": "경영학과",
-                    "major_type": "double_major"
-                }
-            ],
-            status='active'
-        )
-
-        cls.put_data = {
-            'email': 'test@test.com',
-            'password': 'password',
-        }
-
     def test_login_missing_requirements(self):
-        data = {'email': 'test@test.com'}
-        response = self.client.put('/user/login/', data=data)
+        data = {'email': 'member@test.com'}
+        response = self.client.put('/user/login/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         body = response.json()
         self.assertEqual(body['error'], "password missing")
@@ -198,19 +179,19 @@ class LoginTestCase(TestCase):
     def test_login_wrong_request(self):
         data = self.put_data.copy()
         data.update({'password': "false"})
-        response = self.client.put('/user/login/', data=data)
+        response = self.client.put('/user/login/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         body = response.json()
         self.assertEqual(body['error'], "username or password wrong")
 
     def test_login(self):
         data = self.put_data
-        response = self.client.put('/user/login/', data=data)
+        response = self.client.put('/user/login/', data=data, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         body = response.json()
         self.assertIn("id", body)
-        self.assertEqual(body["email"], "test@test.com")
+        self.assertEqual(body["email"], "testuser@test.com")
         self.assertEqual(body["entrance_year"], 2022)
         self.assertEqual(body["full_name"], "홍길동")
         self.assertIn("majors", body)
@@ -218,37 +199,19 @@ class LoginTestCase(TestCase):
         self.assertEqual(body["status"], "active")
         self.assertIn("token", body)
 
-
-class LogoutTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = UserFactory(
-            email='test@test.com',
-            password='password',
-            entrance_year=2022,
-            full_name='홍길동',
-            majors=[
-                {
-                    "major_name": "컴퓨터공학부",
-                    "major_type": "major"
-                },
-                {
-                    "major_name": "경영학과",
-                    "major_type": "double_major"
-                }
-            ],
-            status='active'
-        )
-
-        cls.put_data = {
-            'email': 'test@test.com',
-            'password': 'password',
-        }
-
     def test_logout(self):
-        data = self.put_data
-        response = self.client.put('/user/login/', data=data)
-        token = response.json()["token"]
-
-        response = self.client.get('/user/logout/', Authorization=token)
+        response = self.client.get('/user/logout/', HTTP_AUTHORIZATION=self.user_token)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_me(self):
+        response = self.client.get('/user/me/', HTTP_AUTHORIZATION=self.user_token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        body = response.json()
+        self.assertIn("id", body)
+        self.assertEqual(body["email"], "testuser@test.com")
+        self.assertEqual(body["entrance_year"], 2022)
+        self.assertEqual(body["full_name"], "홍길동")
+        self.assertIn("majors", body)
+        self.assertEqual(len(body["majors"]), 2)
+        self.assertEqual(body["status"], "active")
