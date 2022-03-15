e37a90fc9b376839410e221eef0fc1c36c6018ff
jaejae2374 <jaejae2374@snu.ac.kr>
2022-03-15 21:12:30 +0900

    feature/add_unittests#60

--- snugh/lecture/tests.py
+++ snugh/lecture/tests.py
@@ -959,10 +959,109 @@ class PlanTestCase(TestCase):
         self.assertEqual(data['semesters'][1]['major_elective_credit'], self.p2_semester_2.major_elective_credit)
         self.assertEqual(len(data['semesters'][1]['lectures']), 1)
 
-    
+    # TODO: 아래 테스트 RequirementTest에서 활용하기
+    """
     def test_plan_major_update(self):
-        """
-        Test [PUT] plan/<plan_id>/major/
-        """
+        # Test [PUT] plan/<plan_id>/major/
 
-        pass
+        # update major
+        body = {
+            "post_list": [{
+                "major_name": "심리학과",
+                "major_type": "double_major"
+            }],
+            "delete_list": [{
+                "major_name": "컴퓨터공학부",
+                "major_type": "double_major"
+            }]
+        }
+        response = self.client.put(
+            f"/plan/{self.plan_2.id}/major/",
+            data=body,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=self.user_token,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+
+        self.assertEqual(data['plan_name'], self.plan_2.plan_name)
+        self.assertEqual(len(data['majors']), 2)
+        self.assertEqual(data['majors'][0]['major_name'], self.major_1.major_name)
+        self.assertEqual(data['majors'][0]['major_type'], self.major_1.major_type)
+        self.assertEqual(data['majors'][1]['major_name'], "심리학과")
+        self.assertEqual(data['majors'][1]['major_type'], "double_major")
+        
+        self.assertEqual(len(data['semesters']), 2)
+        self.assertEqual(data['semesters'][0]['year'], self.p2_semester_1.year)
+        self.assertEqual(data['semesters'][0]['semester_type'], self.p2_semester_1.semester_type)
+        self.assertEqual(data['semesters'][0]['major_requirement_credit'], self.p2_semester_1.major_requirement_credit)
+        self.assertEqual(data['semesters'][0]['major_elective_credit'], self.p2_semester_1.major_elective_credit)
+        self.assertEqual(len(data['semesters'][0]['lectures']), 1)
+
+        self.assertEqual(data['semesters'][1]['year'], self.p2_semester_2.year)
+        self.assertEqual(data['semesters'][1]['semester_type'], self.p2_semester_2.semester_type)
+        self.assertEqual(data['semesters'][1]['major_requirement_credit'], self.p2_semester_2.major_requirement_credit)
+        self.assertEqual(data['semesters'][1]['major_elective_credit'], self.p2_semester_2.major_elective_credit)
+        self.assertEqual(len(data['semesters'][1]['lectures']), 1)
+
+        # delete major
+        body = {
+            "delete_list": [{
+                "major_name": "심리학과",
+                "major_type": "double_major"
+            }]
+        }
+        response = self.client.put(
+            f"/plan/{self.plan_2.id}/major/",
+            data=body,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=self.user_token,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+
+        self.assertEqual(data['plan_name'], self.plan_2.plan_name)
+        self.assertEqual(len(data['majors']), 1)
+        self.assertEqual(data['majors'][0]['major_name'], self.major_1.major_name)
+        self.assertEqual(data['majors'][0]['major_type'], "single_major")
+
+        # 400 error
+        body = {
+            "delete_list": [{
+                "major_name": "경영학과",
+                "major_type": "major"
+            }]
+        }
+        response = self.client.put(
+            f"/plan/{self.plan_2.id}/major/",
+            data=body,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=self.user_token,
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data = response.json()
+        self.assertEqual(data['error'], "The number of majors cannot be zero or minus.")
+
+        # add major
+        body = {
+            "post_list": [{
+                "major_name": "컴퓨터공학부",
+                "major_type": "double_major"
+            }]
+        }
+        response = self.client.put(
+            f"/plan/{self.plan_2.id}/major/",
+            data=body,
+            content_type="application/json",
+            HTTP_AUTHORIZATION=self.user_token,
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.json()
+
+        self.assertEqual(data['plan_name'], self.plan_2.plan_name)
+        self.assertEqual(len(data['majors']), 2)
+        self.assertEqual(data['majors'][0]['major_name'], self.major_1.major_name)
+        self.assertEqual(data['majors'][0]['major_type'], "major")
+        self.assertEqual(data['majors'][1]['major_name'], self.major_2.major_name)
+        self.assertEqual(data['majors'][1]['major_type'], self.major_2.major_type)
+    """
