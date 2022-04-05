91765eb97ca43029cd49206663afc4c417e126cc
Jaewan Park <jaejae1112@gmail.com>
2022-04-05 16:48:34 +0900

    refactor: POST /signup/ API

--- snugh/user/serializers.py
+++ snugh/user/serializers.py
@@ -1,6 +1,54 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-from user.models import Major
+from user.models import *
+
+
+class UserCreateSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    token = serializers.SerializerMethodField()
+
+    email = serializers.EmailField(max_length=100)
+    password = serializers.CharField(max_length=100, min_length=6, write_only=True)
+    entrance_year = serializers.IntegerField(min_value=1000, max_value=9999, write_only=True)
+    full_name = serializers.CharField(max_length=30, min_length=2, write_only=True)
+    majors = serializers.ListField(child=serializers.JSONField(), allow_empty=False, write_only=True)
+    status = serializers.ChoiceField(choices=UserProfile.STUDENT_STATUS, write_only=True)
+
+    def get_token(self, user):
+        token, created = Token.objects.get_or_create(user=user)
+        return token.key
+
+    def validate_email(self, value):
+        if User.objects.filter(username=value).exists():
+            raise ValidationError({'email': "Already existing email."})
+        return value
+
+    def validate_major(self, value):
+        for major in value:
+            if major['major_type'] == Major.MAJOR:
+                return value
+        raise ValidationError({'majors': "major_type not_allowed"})
+
+    def validate(self, data):
+        data['username'] = data.get('email')
+        data['first_name'] = data.pop('full_name')
+        return data
+
+    def create(self, validated_data):
+        entrance_year = validated_data.pop('entrance_year')
+        student_status = validated_data.pop('status')
+        major_list = validated_data.pop('majors')
+
+        user = User.objects.create_user(**validated_data)
+        UserProfile.objects.create(user=user, entrance_year=entrance_year, status=student_status)
+
+        for major in major_list:
+            searched_major = Major.objects.get(major_name=major['major_name'], major_type=major['major_type'])
+            UserMajor.objects.create(user=user, major=searched_major)
+
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
--- snugh/user/urls.py
+++ snugh/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from user.views import UserViewSet, MajorViewSet
+from user.views import UserSignUpView, UserViewSet, MajorViewSet
 
 
 app_name = 'user'
@@ -10,5 +10,6 @@ router.register('user', UserViewSet, basename='user')
 router.register('major', MajorViewSet, basename='major')  
 
 urlpatterns = [
+    path('signup/', UserSignUpView.as_view(), name='signup'),
     path('', include((router.urls))),
 ]
