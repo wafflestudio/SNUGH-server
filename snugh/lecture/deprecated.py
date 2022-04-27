0bf609958dee41e935c520e2878a38c1ce12d307
jaejae2374 <jaejae2374@snu.ac.kr>
2022-04-27 15:58:41 +0900

    delete deprecated apis

--- snugh/lecture/deprecated.py
+++ snugh/lecture/deprecated.py
@@ -1,148 +1,127 @@
-# Deprecated Common Functions
-def update_plan_info(plan):
-    # TODO: need to delete
-    # SemesterLecture 모델의 lecture_type 관련 값 업데이트
-    majors = Major.objects.filter(planmajor__plan=plan)
-    entrance_year = plan.user.userprofile.entrance_year
-    semester_lectures = SemesterLecture.objects.filter(semester__plan=plan)
-    for semester_lecture in list(semester_lectures):
-        lecture = semester_lecture.lecture
-        recognized_major, lecture_type = cal_lecture_type(lecture, majors, entrance_year)
-        semester_lecture.recognized_major = recognized_major
-        semester_lecture.lecture_type = lecture_type
-        semester_lecture.save()
+"""
+# Data Generation
+    # POST /lecture/generate_lecturecredit/
+    @action(methods=['POST'], detail=False)
+    @transaction.atomic
+    def generate_lecturecredit(self, request):
+        lectures = Lecture.objects.all()
+        id_cnt = 0 # 이전 item의 id
+        for lecture in lectures:
+            lecture_code = lecture.lecture_code
+            lecturecredits = LectureTmp.objects.filter(lecture_code=lecture_code).order_by('id')
+            if lecturecredits.count() == 0:
+                return Response({"error": "lecture missing"}, status=status.HTTP_400_BAD_REQUEST)
+            elif lecturecredits.count() == 1:
+                LectureCredit.objects.create(lecture=lecture,
+                                             credit=lecturecredits.first().credit,
+                                             start_year=lecturecredits.first().open_year,
+                                             end_year = 10000)
+                id_cnt+=1
+            else:
+                cnt = 0
+                for lecturecredit in lecturecredits:
+                    LectureCredit.objects.create(lecture=lecture,
+                                                 credit=lecturecredit.credit,
+                                                 start_year=lecturecredit.open_year,
+                                                 end_year=10000)
+                    if cnt !=0:
+                        prev_lecturecredit = LectureCredit.objects.get(id=id_cnt)
+                        prev_lecturecredit.end_year = lecturecredit.open_year -1
+                        prev_lecturecredit.save()
+                    cnt +=1
+                    id_cnt+=1
+        return Response(status=status.HTTP_200_OK)
 
-    # Semester 모델의 credit 관련 값 업데이트
-    semesters = Semester.objects.filter(plan=plan)
-    for semester in list(semesters):
-        mr_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                             semesterlecture__lecture_type=SemesterLecture.MAJOR_REQUIREMENT)
-        me_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                             semesterlecture__lecture_type=SemesterLecture.MAJOR_ELECTIVE)
-        t_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                            semesterlecture__lecture_type=SemesterLecture.TEACHING)
-        g_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                            semesterlecture__lecture_type=SemesterLecture.GENERAL)
-        ge_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                             semesterlecture__lecture_type=SemesterLecture.GENERAL_ELECTIVE)
-        mr_credit = 0
-        me_credit = 0
-        g_credit = 0
-        ge_credit = 0
+    # POST /lecture/combine_majorlecture/
+    # 수집된 데이터와 수강신청 사이트의 데이터 합치기(수강신청 사이트 데이터에 없는 강의만 추가)
+    @action(methods=['POST'], detail=False)
+    @transaction.atomic
+    def combine_majorlecture(self, request):
+        cnt_created=0
+        majors = Major.objects.exclude(major_type = Major.SINGLE_MAJOR).exclude(major_type=Major.DOUBLE_MAJOR).exclude(major_type=Major.MINOR)
+        for major in majors:
+            majorlectures_auto_lectures = LectureTmp.objects.filter(open_major=major.major_name).values_list('lecture_code', flat=True).distinct()
+            majorlectures_collected = MajorLectureTmp.objects.filter(major_name=major.major_name)
+            for ml in majorlectures_collected:
+                if ml.lecture_code not in majorlectures_auto_lectures:
+                    LectureTmp.objects.create(lecture_code=ml.lecture_code,
+                                              lecture_name='',
+                                              open_major=ml.major_name,
+                                              open_year=ml.start_year,
+                                              lecture_type=ml.lecture_type,
+                                              is_added=True)
+                    cnt_created+=1
+        return Response(cnt_created, status=status.HTTP_200_OK)
 
-        for lecture in list(mr_lectures):
-            mr_credit += lecture.credit
-        for lecture in list(me_lectures):
-            me_credit += lecture.credit
-        for lecture in list(t_lectures):
-            me_credit += lecture.credit
-        for lecture in list(g_lectures):
-            g_credit += lecture.credit
-        for lecture in list(ge_lectures):
-            ge_credit += lecture.credit
 
-        semester.major_requirement_credit = mr_credit
-        semester.major_elective_credit = me_credit
-        semester.general_credit = g_credit
-        semester.general_elective_credit = ge_credit
-        semester.save()
+    # POST /lecture/generate_majorlecture/
+    @action(methods=['POST'], detail=False)
+    @transaction.atomic
+    def generate_majorlecture(self, request):
+        created_cnt = 0
+        deleted_cnt = 0
+        auto_generated_cnt = 0
+        # generate majorlecture object from lecturetmp, find start_year, end_year
+        majors = Major.objects.all()
+        id_cnt = 0
+        for major in majors:
+            open_department = Department.objects.get(majordepartment__major = major).department_name
+            if major.major_name == 'none':
+                majorlectures_lecture_codes = LectureTmp.objects.filter(open_major=major.major_name).values_list('lecture_code', flat=True).distinct()
+            else:
+                majorlectures_lecture_codes = LectureTmp.objects.filter(open_major=major.major_name, open_department=open_department).values_list('lecture_code', flat=True).distinct()
+            for lecture_code in majorlectures_lecture_codes:
+                if major.major_name == 'none':
+                    majorlectures = LectureTmp.objects.filter(open_major=major.major_name,
+                                                              lecture_code=lecture_code).order_by('id')
+                else:
+                    majorlectures = LectureTmp.objects.filter(open_major = major.major_name, open_department=open_department, lecture_code = lecture_code).order_by('id')
+                if majorlectures.count() == 0:
+                    return Response({"error": "missing"}, status=status.HTTP_400_BAD_REQUEST)
+                elif majorlectures.count() == 1:
+                    MajorLecture.objects.create(major = major,
+                                                lecture = Lecture.objects.get(lecture_code = lecture_code),
+                                                start_year = majorlectures.first().open_year,
+                                                end_year = 10000,
+                                                lecture_type = majorlectures.first().lecture_type)
+                    id_cnt += 1;
+                else:
+                    cnt = 0
+                    for majorlecture in majorlectures:
+                        MajorLecture.objects.create(major=major,
+                                                    lecture=Lecture.objects.get(lecture_code=lecture_code),
+                                                    start_year=majorlecture.open_year,
+                                                    end_year=10000,
+                                                    lecture_type=majorlecture.lecture_type)
+                        if cnt != 0:
+                            prev_majorlecture = MajorLecture.objects.get(id=id_cnt)
+                            if prev_majorlecture.start_year == majorlecture.open_year:
+                                prev_majorlecture.end_year = majorlecture.open_year
+                            else:
+                                prev_majorlecture.end_year = majorlecture.open_year - 1
+                            prev_majorlecture.save()
+                        cnt += 1
+                        id_cnt += 1
+        created_cnt = id_cnt
 
-    # PlanRequirement 모델의 earned_credit, is_fulfilled 업데이트
-    majors = Major.objects.filter(planmajor__plan=plan)
-    for major in list(majors):
-        mr_r = Requirement.objects.get(major=major, requirement_type=Requirement.MAJOR_REQUIREMENT)
-        me_r = Requirement.objects.get(major=major, requirement_type=Requirement.MAJOR_ELECTIVE)
-        g_r = Requirement.objects.get(major=major, requirement_type=Requirement.GENERAL)
-        ge_r = Requirement.objects.get(major=major, requirement_type=Requirement.GENERAL_ELECTIVE)
+        # generate majorlecture object for major = none, lecture_type = 'major_requirement' or 'major_elective'
+        null_majorlectures = MajorLecture.objects.filter(major = Major.objects.get(major_name = 'none'), lecture_type = Lecture.MAJOR_REQUIREMENT)|MajorLecture.objects.filter(major = Major.objects.get(major_name = 'none'), lecture_type = Lecture.MAJOR_ELECTIVE)
+        for null_majorlecture in null_majorlectures:
+            null_majorlecture_src = LectureTmp.objects.get(open_major = null_majorlecture.major.major_name, lecture_code= null_majorlecture.lecture.lecture_code, open_year = null_majorlecture.start_year, lecture_type = null_majorlecture.lecture_type)
+            open_department = Department.objects.get(department_name=null_majorlecture_src.open_department)
+            majors = Major.objects.filter(majordepartment__department=open_department)
+            for major in majors:
+                existing_majorlectures = MajorLecture.objects.filter(major = major, lecture = null_majorlecture.lecture)
+                if existing_majorlectures.count() == 0:
+                    MajorLecture.objects.create(major=major,
+                                                lecture=null_majorlecture.lecture,
+                                                start_year=null_majorlecture.start_year,
+                                                end_year=null_majorlecture.end_year,
+                                                lecture_type=null_majorlecture.lecture_type)
+                    auto_generated_cnt += 1
+            null_majorlecture.delete()
+            deleted_cnt += 1
 
-        mr_pr = PlanRequirement.objects.get(plan=plan, requirement__major=major,
-                                            requirement__requirement_type=Requirement.MAJOR_REQUIREMENT)
-        me_pr = PlanRequirement.objects.get(plan=plan, requirement__major=major,
-                                            requirement__requirement_type=Requirement.MAJOR_ELECTIVE)
-        g_pr = PlanRequirement.objects.get(plan=plan, requirement__major=major,
-                                           requirement__requirement_type=Requirement.GENERAL)
-        ge_pr = PlanRequirement.objects.get(plan=plan, requirement__major=major,
-                                            requirement__requirement_type=Requirement.GENERAL_ELECTIVE)
-        mr_credit = 0
-        me_credit = 0
-        g_credit = 0
-        ge_credit = 0
-        for semester in list(semesters):
-            mr_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                                 semesterlecture__pivot_major=major,
-                                                 semesterlecture__lecture_type=SemesterLecture.MAJOR_REQUIREMENT)
-            me_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                                 semesterlecture__pivot_major=major,
-                                                 semesterlecture__lecture_type=SemesterLecture.MAJOR_ELECTIVE)
-            g_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                                semesterlecture__pivot_major=major,
-                                                semesterlecture__lecture_type=SemesterLecture.GENERAL)
-            ge_lectures = Lecture.objects.filter(semesterlecture__semester=semester,
-                                                 semesterlecture__pivot_major=major,
-                                                 semesterlecture__lecture_type=SemesterLecture.GENERAL_ELECTIVE)
-            for lecture in list(mr_lectures):
-                mr_credit += lecture.credit
-            for lecture in list(me_lectures):
-                me_credit += lecture.credit
-            for lecture in list(g_lectures):
-                g_credit += lecture.credit
-            for lecture in list(ge_lectures):
-                ge_credit += lecture.credit
-
-        mr_pr.earned_credit = mr_credit
-        mr_pr.save()
-        me_pr.earned_credit = me_credit
-        me_pr.save()
-        g_pr.earned_credit = g_credit
-        g_pr.save()
-        ge_pr.earned_credit = ge_credit
-        ge_pr.save()
-
-        if mr_r.required_credit <= mr_pr.earned_credit:
-            mr_pr.is_fulfilled = True
-            mr_pr.save()
-        if me_r.required_credit <= me_pr.earned_credit:
-            me_pr.is_fulfilled = True
-            me_pr.save()
-        if g_r.required_credit <= g_pr.earned_credit:
-            g_pr.is_fulfilled = True
-            g_pr.save()
-        if ge_r.required_credit <= ge_pr.earned_credit:
-            ge_pr.is_fulfilled = True
-            ge_pr.save()
-
-def cal_lecture_type(lecture, majors, entrance_year):
-    result_major = None
-    result_lecture_type = None
-    for major in list(majors):
-        try:
-            majorlecture = MajorLecture.objects.get(major=major,
-                                                    lecture=lecture,
-                                                    start_year__lte=entrance_year,
-                                                    end_year__gte=entrance_year)
-            if cal_priority_lt(majorlecture.lecture_type) > cal_priority_lt(result_lecture_type):
-                result_major = majorlecture.major
-                result_lecture_type = majorlecture.lecture_type
-        except MajorLecture.DoesNotExist:
-            pass
-    return result_major, result_lecture_type
-
-def cal_priority_lt(lecture_type):
-    switcher = {
-        SemesterLecture.MAJOR_REQUIREMENT: 4,
-        SemesterLecture.MAJOR_ELECTIVE: 3,
-        SemesterLecture.TEACHING: 3,
-        SemesterLecture.GENERAL: 2,
-        SemesterLecture.GENERAL_ELECTIVE: 1
-    }
-    return switcher.get(lecture_type, -1) # -1 is error
-
-def lecture_type_to_int(semesterlecture):
-    switcher = {
-        SemesterLecture.MAJOR_REQUIREMENT : 1,
-        SemesterLecture.MAJOR_ELECTIVE : 2,
-        SemesterLecture.TEACHING : 3,
-        SemesterLecture.GENERAL : 4,
-        SemesterLecture.GENERAL_ELECTIVE : 5
-    }
-    return switcher.get(semesterlecture, -1) # -1 is error
+        data = [created_cnt, deleted_cnt, auto_generated_cnt]
+        return Response(data, status=status.HTTP_200_OK)
+"""
