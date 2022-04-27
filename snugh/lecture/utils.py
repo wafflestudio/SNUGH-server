5353e7870d6137e6378b22593b75b8d4f1672833
jaejae2374 <jaejae2374@snu.ac.kr>
2022-04-27 16:15:19 +0900

    destroy unused utils, import line

--- snugh/lecture/utils.py
+++ snugh/lecture/utils.py
@@ -6,37 +6,6 @@ from django.db.models import Case, When, Value, IntegerField
 from snugh.exceptions import NotOwner, NotFound
 from typing import List
 
-# Common Functions
-def add_credits(semesterlecture):
-    semester = semesterlecture.semester
-    if semesterlecture.lecture_type == MAJOR_REQUIREMENT:
-        semester.major_requirement_credit += semesterlecture.credit
-    elif semesterlecture.lecture_type2 == MAJOR_REQUIREMENT:
-        semester.major_requirement_credit += semesterlecture.credit
-    elif semesterlecture.lecture_type == MAJOR_ELECTIVE or semesterlecture.lecture_type == TEACHING:
-        semester.major_elective_credit += semesterlecture.credit
-    elif semesterlecture.lecture_type == GENERAL:
-        semester.general_credit += semesterlecture.credit
-    elif semesterlecture.lecture_type == GENERAL_ELECTIVE:
-        semester.general_elective_credit += semesterlecture.credit
-    return semester
-
-
-def subtract_credits(semesterlecture):
-    semester = semesterlecture.semester
-    if semesterlecture.lecture_type == MAJOR_REQUIREMENT:
-        semester.major_requirement_credit -= semesterlecture.credit
-    elif semesterlecture.lecture_type2 == MAJOR_REQUIREMENT:
-        semester.major_requirement_credit -= semesterlecture.credit
-    elif semesterlecture.lecture_type == MAJOR_ELECTIVE or semesterlecture.lecture_type == TEACHING:
-        semester.major_elective_credit -= semesterlecture.credit
-    elif semesterlecture.lecture_type == GENERAL:
-        semester.general_credit -= semesterlecture.credit
-    elif semesterlecture.lecture_type == GENERAL_ELECTIVE:
-        semester.general_elective_credit -= semesterlecture.credit
-    return semester
-
-
 def add_semester_credits(semesterlecture: SemesterLecture, semester: Semester) -> Semester:
     """ Add SemesterLecture's credits to Semester credits. """
     if semesterlecture.lecture_type == MAJOR_REQUIREMENT:
--- snugh/lecture/views.py
+++ snugh/lecture/views.py
@@ -5,17 +5,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from lecture.models import * 
 from lecture.serializers import *
-from lecture.utils import lecturetype_history_generator
-from snugh.lecture.utils import credit_history_generator
 from user.models import *
 from requirement.models import *
 from django.core.paginator import Paginator
 from django.db.models.functions import Length
 from snugh.permissions import IsOwnerOrCreateReadOnly
 from snugh.exceptions import DuplicationError, NotOwner
-from lecture.utils import add_semester_credits, sub_semester_credits
+from lecture.utils import *
 from lecture.const import *
-from lecture.utils import update_lecture_info
 
 
 class PlanViewSet(viewsets.GenericViewSet, generics.RetrieveUpdateDestroyAPIView):
