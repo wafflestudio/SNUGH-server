53294fad8e173747392fb9c3f0aeaefa814cff0c
jaejae2374 <80879249+jaejae2374@users.noreply.github.com>
2022-05-20 13:44:56 +0900

    Update apps.py

--- snugh/bug_report/apps.py
+++ snugh/bug_report/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-
+import os
+from snugh.settings.base import BASE_DIR
 
 class BugReportConfig(AppConfig):
     name = 'bug_report'
+    path = os.path.join(BASE_DIR, 'bug_report')
