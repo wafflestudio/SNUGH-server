4e57d4aaad4f3fd60367865f72c049aa8e14e927
jaejae2374 <80879249+jaejae2374@users.noreply.github.com>
2022-05-20 13:43:11 +0900

    Update apps.py

--- snugh/history/apps.py
+++ snugh/history/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+import os
+from snugh.settings.base import BASE_DIR
 
 class HistoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'history'
+    path = os.path.join(BASE_DIR, 'history')
