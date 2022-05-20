d9aa391a2f92389684cd8805d13743ceb4fdd495
jaejae2374 <80879249+jaejae2374@users.noreply.github.com>
2022-05-20 13:41:02 +0900

    Update apps.py

--- snugh/requirement/apps.py
+++ snugh/requirement/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-
+import os
+from snugh.settings.base import BASE_DIR
 
 class RequirementConfig(AppConfig):
     name = 'requirement'
+    path = os.path.join(BASE_DIR, 'requirement')
