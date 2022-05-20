4becf8e1e02af11796b7d5368cfd598794571156
jaejae2374 <80879249+jaejae2374@users.noreply.github.com>
2022-05-20 13:44:32 +0900

    Update apps.py

--- snugh/user/apps.py
+++ snugh/user/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-
+import os
+from snugh.settings.base import BASE_DIR
 
 class UserConfig(AppConfig):
     name = 'user'
+    path = os.path.join(BASE_DIR, 'user')
