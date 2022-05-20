092837d81eb1249cca48090b4ffa36e0e20affdb
jaejae2374 <80879249+jaejae2374@users.noreply.github.com>
2022-05-20 13:43:41 +0900

    Update apps.py

--- snugh/plan/apps.py
+++ snugh/plan/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+import os
+from snugh.settings.base import BASE_DIR
 
 class PlanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'plan'
+    path = os.path.join(BASE_DIR, 'requirement')
+    
