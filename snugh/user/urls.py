c20813807185cd02b8c335af4509d67b1fce83b6
Jaewan Park <jaejae1112@gmail.com>
2022-06-20 05:43:47 +0900

    fix: signup, login, logout urls

--- snugh/user/urls.py
+++ snugh/user/urls.py
@@ -9,8 +9,8 @@ router = SimpleRouter()
 router.register('user', UserViewSet, basename='user')  
 
 urlpatterns = [
-    path('signup/', UserSignUpView.as_view(), name='signup'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('user/', UserSignUpView.as_view(), name='signup'),
+    path('user/login/', UserLoginView.as_view(), name='login'),
+    path('user/logout/', UserLogoutView.as_view(), name='logout'),
     path('', include((router.urls))),
 ]
